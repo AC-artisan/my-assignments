c953ed146593194e60ba9ea054f2568370f6ab21
AC-artisan <asc.studies@gmail.com>
2026-09-17 09:44:27 -0700

    Add files via upload

---
+++ about_me.py
@@ -0,0 +1,31 @@
+#My about me exercise
+
+print("~" * 75) #aesthetic border
+print("                         Student Profile")
+print("~" * 75)
+
+#My student profile information
+
+first_name = "Aja"
+last_name = "Cunningham"
+age = "28"
+state = "California"
+career = "LNMT" #current career
+why_code = "I want to learn how to code because I love languages and solving problems. I also want to build amazing things." #why I'm learning to code
+fun_fact = "I've learned to speak different languages, my favourites being Mandarin and Python." #a fun fact about me
+
+print(f"Name:             {first_name} {last_name}")
+print(f"Age:              {age}")
+print(f"State:            {state}")
+print(f"Current Career:   {career}")
+
+print("\n") #empty line for aesthetic purposes
+
+print("Why I'm learning to code:")
+print(why_code)
+
+print("\n") #empty line for aesthetic purposes
+
+print(f"Fun fact: {fun_fact}")
+
+print("~" * 75) #aesthetic border
